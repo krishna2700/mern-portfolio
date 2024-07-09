7ec5a435c43647f713495543f8b91e8f7c9b0d9a
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-09 18:06:25 +0530

    React and tailwind setup

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App flex h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold">Jai Swaminarayan</h1>
+    </div>
+  );
+}
+
+export default App;
