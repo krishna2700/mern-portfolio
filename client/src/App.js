659b8e84266b7306a2f5ff4025347e4d185097fd
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-14 00:08:47 +0530

    loader done frondtend completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { useState } from "react";
+import Loader from "./components/Loader";
 
 function App() {
+  const [showLoading, setShowLoading] = useState(false);
   return (
     <BrowserRouter>
+      {showLoading ? <Loader /> : null}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
