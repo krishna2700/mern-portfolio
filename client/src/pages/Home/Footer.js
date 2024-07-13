4a5d954c951c602c1ca20487dc503079a34dceab
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-13 21:51:04 +0530

    Footer

---
+++ client/src/pages/Home/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-10">
+      <div className="h-[1px] w-full bg-gray-700"></div>
+      <div className="flex items-center justify-center flex-col mt-10 opacity-70">
+        <h1 className="text-white">Designed and developed by</h1>
+        <h1 className="text-white">
+          <span className="text-white">Krishna Ruparelia</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -6,6 +6,7 @@ import Experience from "./Experience";
 import ProjectsFile from "./Projects";
 import Education from "./Education";
 import Contact from "./Contact";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
         <ProjectsFile />
         <Education />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
