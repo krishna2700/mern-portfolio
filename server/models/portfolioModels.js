ba4d345a0b96e93392a869d1c99ba4ac1c42924a
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-16 13:18:50 +0530

    Creating models

---
+++ server/models/portfolioModels.js
@@ -0,0 +1,140 @@
+const mongoose = require("mongoose");
+
+const introSchema = new mongoose.Schema({
+  welcomeText: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const aboutSchema = new mongoose.Schema({
+  lottieURL: {
+    type: String,
+    required: true,
+  },
+  description1: {
+    type: String,
+    required: true,
+  },
+  description2: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: Array,
+    required: true,
+  },
+});
+
+const experienceSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  period: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const projectsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  techStack: {
+    type: Array,
+    required: true,
+  },
+});
+
+const educationSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  period: {
+    type: String,
+    required: true,
+  },
+  school: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = {
+  into: mongoose.model("intro", introSchema),
+  about: mongoose.model("about", aboutSchema),
+  experience: mongoose.model("experience", experienceSchema),
+  projects: mongoose.model("projects", projectsSchema),
+  education: mongoose.model("education", educationSchema),
+  contact: mongoose.model("contact", contactSchema),
+};
