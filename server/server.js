fa735cacbd81a6a6cc87d8aa263c7e16ca4cea0b
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-20 14:01:01 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -8,14 +8,6 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoutes);
 
 const PORT = process.env.PORT || 3690;
-const path = require("path");
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 
 connectToDB();
 
