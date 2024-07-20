d452b77d36a0fd06fac7bf7647aba75bd667db37
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-20 14:14:04 +0530

    build

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const connectToDB = require("./config/dbConfig");
 const portfolioRoutes = require("./routes/portfolioRoutes");
 
@@ -8,6 +9,13 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoutes);
 
 const PORT = process.env.PORT || 3690;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 connectToDB();
 
