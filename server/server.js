2a4d349f75edba05e99b228747959ca867f33cd6
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-20 15:27:22 +0530

    issues resolved

--- server/server.js
+++ server/server.js
@@ -32,14 +32,6 @@ app.use("/api/portfolio", portfolioRoutes);
 
 const PORT = process.env.PORT || 3690;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 connectToDB();
 
 app.listen(PORT, () => {
